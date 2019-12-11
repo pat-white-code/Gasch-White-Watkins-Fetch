9ac6a23f79eb4d61e91ecff4e261b2134756c3d8
YOUR USERNAMErobpwatkins <robpwatkins@gmail.com>
2019-12-10 20:59:55 -0600

    yeahhhh

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
+let arrayOfPosts;
+
 window.onload = function() {
   getPosts();
 }
 
 const getPosts = () => {
-  fetch('https://randomuser.me/api/')
+  fetch('https://randomuser.me/api/?results=100')
     .then(res => res.json())
-    .then(posts => arrayOfPosts = posts)
+    .then(posts => {
+      arrayOfPosts = posts.results
+      document.getElementById('user').innerHTML = `${arrayOfPosts[0].name.first}`;
+      console.log(arrayOfPosts[0].name);
+    })
 }
+
+
+// console.log(arrayOfPosts);
+// document.getElementById('allPosts').innerHTML = arrayOfPosts;
