32d6ab89c9ceca55522a7ada15e5031fb11ed7cf
dgasch512 <gaschdav208@gmail.com>
2019-12-14 14:57:50 -0600

    Functions and styled lightly

--- dg:fetch/main.js
+++ dg:fetch/main.js
@@ -1,23 +1,15 @@
+// define an array of users
 let arrOfUsers = [];
-
+// get users when page loads
 window.onload = function() {
   getUser();
 }
-
+// get users from api
 const getUser = () => {
   fetch('https://randomuser.me/api/?results=10')
   .then(res => res.json())
   .then(userArr => {
-  // };
-
-  // function getFive() {
-  //   fetch('https://randomuser.me/api/?results=5')
-  // .then(res => res.json())
-  // .then(resArr => arrOfUsers => resArr)
-  // };
-    
-  // const displayUsers = (arr) => {
-    // arrOfUsers = users;
+    // sort results into variables
       arrOfUsers = userArr.results;
       arrOfUsers.map(user => {
         let userThis = document.createElement('div');
@@ -30,14 +22,16 @@ const getUser = () => {
         let userEmail = document.createElement('li');
         let moreUser = document.createElement('button'); 
         let profileName = document.createElement('li')
+        // display result variables for each user
       userProfile.appendChild(userThis);
-      userImg.src = user.picture.thumbnail;
+      userImg.src = user.picture.medium;
       userProfile.appendChild(userImg);
       userName.innerHTML = `${user.name.first} ${user.name.last}`;
       userProfile.appendChild(userName);
       userInfo.innerHTML = `${user.location.city}, ${user.location.state}`;
       userProfile.appendChild(userInfo);
       userProfile.appendChild(moreUser);
+      // create button to show more info
       moreUser.innerHTML = `More info`;
       moreUser.addEventListener('click', function () {
         userProfile.removeChild(moreUser);
