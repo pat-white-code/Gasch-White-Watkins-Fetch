c332acba77d946a7605110a6c3873539a7d8e183
Rob W <robpwatkins@gmail.com>
2019-12-19 16:23:22 -0600

    lets see here

--- app.js
+++ app.js
@@ -1,96 +1,82 @@
 const assert = require('assert')
-// console.log('app.js here...');
+
+console.log('app.js here...');
 
 let userArr = [];
-const getPosts = () => {
-  fetch('https://randomuser.me/api/?results=5')
-  .then(res => res.json())
-  // .then(json => {userArr = json.results.map(user=> user)})
-  .then(json => displayUsers(json.results))
+const getPosts = (fetch, num) => {
+fetch('https://randomuser.me/api/?results='+num)
+.then(res => res.json())
+// .then(json => {userArr = json.results.map(user=> user)})
+.then(json => displayUsers(json.results))
 };
 
 displayUsers = (arr) => {
-  arr.forEach(user => {
-    let userSection = document.getElementById('users');
-    let userDiv = document.createElement('div');
-    let userImg = document.createElement('img');
-    userImg.src = user.picture.thumbnail;
-    let userh1 = document.createElement('h1');
-    let userBtn = document.createElement('button');
-    userBtn.innerText = 'Reveal Gender';
-    let userGender = document.createElement('p')
-    userGender.innerText = `${user.gender}`
-    userBtn.addEventListener('click', ()=>{
-      userDiv.appendChild(userGender)
-    })
-    userh1.innerText = `${user.name.first} ${user.name.last}`
-    let userP = document.createElement('p');
-    userP.innerText = `${user.email}`;
-    userDiv.appendChild(userh1);
-    userDiv.appendChild(userImg);
-    userDiv.appendChild(userP);
-    userSection.appendChild(userDiv);
-    userSection.appendChild(userBtn);
-  })
+arr.forEach(user => {
+let userSection = document.getElementById('users');
+let userDiv = document.createElement('div');
+let userImg = document.createElement('img');
+userImg.src = user.picture.thumbnail;
+let userh1 = document.createElement('h1');
+let userBtn = document.createElement('button');
+userBtn.innerText = 'Reveal Gender';
+let userGender = document.createElement('p')
+userGender.innerText = `${user.gender}`
+userBtn.addEventListener('click', ()=>{
+userDiv.appendChild(userGender)
+})
+userh1.innerText = `${user.name.first} ${user.name.last}`
+let userP = document.createElement('p');
+userP.innerText = `${user.email}`;
+userDiv.appendChild(userh1);
+userDiv.appendChild(userImg);
+userDiv.appendChild(userP);
+userSection.appendChild(userDiv);
+userSection.appendChild(userBtn);
+})
 }
 // window.onLoad = getPosts();
 
 
 // fiveUsers
-//   .then(res => res.json())
-//   .then(json => console.log(json));
+// .then(res => res.json())
+// .then(json => console.log(json));
 
 
 
 describe('getPosts', () => {
-  it('calls fetch with the correct url', () => {
-  const fakeFetch = url => {
-    assert(
-      url ===
-      'https://randomuser.me/api?results=123'
-    )
-    return new Promise(function(resolve){
+it('calls fetch with the correct url', () => {
+const fakeFetch = url => {
+assert(
+url ===
+'https://randomuser.me/api/?results=123'
+)
+return new Promise(function(resolve) {
 
-    })
-  }
-  getPosts(fakeFetch, 123)
 })
 }
+getPosts(fakeFetch, 123)
+})
 
-// function getAnimals(fetch, id) {
-//   return fetch('http://api.animalfarmgame.com/animals/' + id)
-//     .then(response => response.json())
-//     .then(data => data.results[0])
-// }
-
-// describe('getAnimals', () => {
-//   it('calls fetch with the correct url', () => {
-//     const fakeFetch = url => {
-//       assert(
-//         url ===
-//         'http://api.animalfarmgame.com/animals/123'
-//       )
-//       return new Promise(function(resolve) {
-
-//       })
-//     }
-//     getAnimals(fakeFetch, 123)
-//   })
-
-//   it('parses the response of fetch correctly', (done) => {
-//     const fakeFetch = () => {
-//       return Promise.resolve({
-//         json: () => Promise.resolve({
-//           results: [
-//             { name: 'fluffykins' }
-//           ]
-//         })
-//       })
-//     }
-//     getAnimals(fakeFetch, 12345)
-//       .then(result => {
-//         assert(result.name === 'fluffykins')
-//         done()
-//       })
-//   })
-// })
+it('parses the response of fetch correctly', (done) => {
+const fakeFetch = () => {
+return Promise.resolve({
+json: () => Promise.resolve({
+results: [
+{
+cell: "633-586-155",
+dob: {date: "1966-03-28T00:10:47.921Z", age: 53},
+email: "valentin.alonso@example.com",
+gender: "male",
+id: {name: "DNI", value: "05329900-G"},
+}
+]
+})
+})
+}
+getPosts(fakeFetch, 1)
+.then(result => {
+assert(result.cell === "633-586-155")
+done()
+})
+})
+})
